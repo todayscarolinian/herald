486caa68689148e018311a47249560091ff9613f
Neal Peteros <n.peteros2003@gmail.com>
2026-03-10 01:00:21 +0800

    [INTEG-1] Shared Configuration & Code Quality (#2)

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
+import tseslint from "typescript-eslint";
+
 import { config as baseConfig } from "./base.js";
 
 /**
@@ -11,29 +12,44 @@ import { config as baseConfig } from "./base.js";
  *
  * @type {import("eslint").Linter.Config[]} */
 export const config = [
-  ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser,
-      },
+    ...baseConfig,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        languageOptions: {
+            ...pluginReact.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.serviceworker,
+                ...globals.browser,
+            },
+        },
     },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
+    {
+        plugins: {
+            "react-hooks": pluginReactHooks,
+        },
+        settings: { react: { version: "detect" } },
+        rules: {
+            ...pluginReactHooks.configs.recommended.rules,
+            // React scope no longer necessary with new JSX transform
+            "react/react-in-jsx-scope": "off",
+
+            // TypeScript handles prop types
+            "react/prop-types": "off",
+
+            // Security and best practices
+            "react/jsx-no-target-blank": "error",
+            "react/jsx-key": "error",
+            "react/self-closing-comp": "warn",
+            "react/jsx-boolean-value": ["warn", "never"],
+            "react/jsx-curly-brace-presence": ["warn", { "props": "never", "children": "never" }],
+            "react/no-array-index-key": "warn",
+            "react/no-unstable-nested-components": "error",
+
+            // Hooks
+            "react-hooks/exhaustive-deps": "warn",
+        },
     },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-    },
-  },
 ];
