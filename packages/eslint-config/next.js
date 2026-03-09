a5ba05e492f53b88666237edc59b6edffb0d1e46
Seaside <n.peteros2003@gmail.com>
2026-03-10 00:51:39 +0800

    chore: configured ESLint configurations and added pre-commit hooks for linting

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
+import pluginNext from "@next/eslint-plugin-next";
 import { globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
-import pluginNext from "@next/eslint-plugin-next";
+import tseslint from "typescript-eslint";
+
 import { config as baseConfig } from "./base.js";
 
 /**
@@ -14,44 +15,62 @@ import { config as baseConfig } from "./base.js";
  * @type {import("eslint").Linter.Config[]}
  * */
 export const nextJsConfig = [
-  ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  {
-    ...pluginReact.configs.flat.recommended,
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
-      },
+    ...baseConfig,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.recommended,
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
+    {
+        ...pluginReact.configs.flat.recommended,
+        languageOptions: {
+            ...pluginReact.configs.flat.recommended.languageOptions,
+            globals: {
+                ...globals.serviceworker,
+            },
+        },
     },
-  },
-  {
-    plugins: {
-      "@next/next": pluginNext,
+    {
+        plugins: {
+            "@next/next": pluginNext,
+        },
+        rules: {
+            ...pluginNext.configs.recommended.rules,
+            ...pluginNext.configs["core-web-vitals"].rules,
+        },
     },
-    rules: {
-      ...pluginNext.configs.recommended.rules,
-      ...pluginNext.configs["core-web-vitals"].rules,
+    {
+        plugins: {
+            "react-hooks": pluginReactHooks,
+        },
+        settings: { react: { version: "detect" } },
+        rules: {
+            ...pluginReactHooks.configs.recommended.rules,
+            // React scope no longer necessary with new JSX transform
+            "react/react-in-jsx-scope": "off",
+
+            // TypeScript handles prop types
+            "react/prop-types": "off",
+
+            // Security and best practices
+            "react/jsx-no-target-blank": "error",
+            "react/jsx-key": "error",
+            "react/self-closing-comp": "warn",
+            "react/jsx-boolean-value": ["warn", "never"],
+            "react/jsx-curly-brace-presence": ["warn", { "props": "never", "children": "never" }],
+            "react/no-array-index-key": "warn",
+            "react/no-unstable-nested-components": "error",
+
+            // Hooks
+            "react-hooks/exhaustive-deps": "warn",
+
+            // Next.js specific
+            "@next/next/no-html-link-for-pages": "error",
+        },
     },
-  },
-  {
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-    },
-  },
 ];
