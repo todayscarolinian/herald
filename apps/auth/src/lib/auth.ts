d7c4fe52117d291c2e8ee6a301847e10570d7010
alessaaandraaa <24100314@usc.edu.ph>
2026-03-12 23:19:16 +0800

    feat: set up herald auth microservice

--- apps/auth/src/lib/auth.ts
+++ apps/auth/src/lib/auth.ts
@@ -1,10 +1,5 @@
 import { betterAuth } from 'better-auth'
 
-/*export const auth = betterAuth({
-  baseURL: "http://localhost:3000/",
-  emailAndPassword: { enabled: true },
-});*/
-
 export const auth = betterAuth({
   baseURL: 'http://localhost:3000/',
   emailAndPassword: { enabled: true },
