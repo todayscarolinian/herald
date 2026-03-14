be48d51ed6a0842a2eb9582a06ca165d9763d31f
Seaside <n.peteros2003@gmail.com>
2026-03-15 06:49:00 +0800

    refactor: clean up ESLint configuration and added VERCEL_URL env in turbo.json

--- apps/core/eslint.config.mjs
+++ apps/core/eslint.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import { nextJsConfig } from "@repo/eslint-config/next-js"
 
 const eslintConfig = defineConfig([
     ...nextVitals,
