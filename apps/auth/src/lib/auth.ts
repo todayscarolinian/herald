ead3b1c37500821468acedff87ef8cb5eb1b2a81
Seaside <n.peteros2003@gmail.com>
2026-03-15 07:51:12 +0800

    feat: add additional user fields

--- apps/auth/src/lib/auth.ts
+++ apps/auth/src/lib/auth.ts
@@ -18,7 +18,10 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
-  emailAndPassword: { enabled: true },
+    emailAndPassword: {
+        enabled: true,
+      
+   },
   advanced: {
     cookiePrefix: SESSION_COOKIE_NAME,
     crossSubDomainCookies: {
@@ -54,6 +57,9 @@ export const auth = betterAuth({
       firstName: { type: 'string' },
       lastName: { type: 'string' },
       middleName: { type: 'string', required: false },
+      positions: { type: 'string[]', defaultValue: [], required: false, input: false },
+      disabled: { type: 'boolean', defaultValue: false, required: false, input: false },
+      mustChangePassword: { type: 'boolean', defaultValue: false, required: true, input: false },
     },
   },
   callbacks: {
