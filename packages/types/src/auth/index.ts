dd685d1b7988b86ca6f21ec1eb5eafe5ad576cd1
mykroble <mykroble@gmail.com>
2026-03-31 21:21:25 +0800

    feat: added change password request

--- packages/types/src/auth/index.ts
+++ packages/types/src/auth/index.ts
@@ -35,6 +35,12 @@ export interface ResetPasswordRequest {
   confirmPassword: string
 }
 
+export interface ChangePasswordRequest {
+  currentPassword: string
+  newPassword: string
+  confirmPassword: string
+}
+
 export interface APIResponse<T = unknown> {
   success: boolean
   data?: T
